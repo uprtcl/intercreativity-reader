db97099491a50921d3760e3445fbbdd2dace4379
Pepo <pepo@uprtcl.io>
2020-11-10 22:23:18 +0100

    working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,70 @@
+import { EveesReader } from '@uprtcl/evees-reader';
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import { wrapperP } from '..';
 
 import './../assets/scss/App.scss';
 
 interface IProps {}
 
 interface IState {
-  perspectiveId?: string;
+  loading: boolean;
+  reading: boolean;
+  perspectiveId: string;
+  dataRead: any;
 }
 
 class App extends React.Component<IProps, IState> {
+  reader!: EveesReader;
+
   constructor(props: IProps) {
     super(props);
-    this.state = { perspectiveId: '' };
+    this.state = {
+      loading: true,
+      reading: false,
+      perspectiveId: '',
+      dataRead: {},
+    };
   }
 
   async componentWillMount() {
-    this.setState((state) => ({ perspectiveId: 'hello' }));
+    const wrapper = await wrapperP;
+    await wrapper.load();
 
-    // console.log('Perspective created', perspectiveId);
+    this.reader = new EveesReader(wrapper.remotes, wrapper.ipfsStore);
+
+    this.setState((state) => ({ loading: false }));
+  }
+
+  async read() {
+    this.setState({ reading: true });
+    const dataRead = await this.reader.resolve(this.state.perspectiveId);
+    this.setState({ dataRead });
+    this.setState({ reading: false });
+  }
+
+  setPerspectiveId(perspectiveId: string) {
+    this.setState({ perspectiveId });
   }
 
   public render() {
-    return <div>{this.state.perspectiveId}</div>;
+    if (this.state.loading) {
+      return <p>loading...</p>;
+    }
+    return (
+      <div>
+        <input
+          onChange={(event) => this.setPerspectiveId(event.target.value)}
+        />
+        <button onClick={() => this.read()}>Read</button>
+        <br />
+        {this.state.reading ? (
+          <p>reading...</p>
+        ) : (
+          <pre>{JSON.stringify(this.state.dataRead, undefined, 2)}</pre>
+        )}
+      </div>
+    );
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,34 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import IPFS from 'ipfs';
+import * as IPFS from 'ipfs';
 
 import App from './components/App';
 
-import { EveesReader } from '@uprtcl/evees-reader';
 import { EveesPolkadotWrapper } from '@uprtcl/evees-polkadot';
 import { env } from './env';
 
-export let wrapper: EveesPolkadotWrapper;
-export let reader: EveesReader;
+export let wrapperP: Promise<EveesPolkadotWrapper>;
 
 (async function () {
-  const ipfs = IPFS.create();
-  wrapper = new EveesPolkadotWrapper(ipfs, env.pinner);
-  await wrapper.load();
-  reader = new EveesReader(this.wrapper.remotes, this.wrapper.ipfsStore);
+  const ipfsJSConfig = {
+    preload: { enabled: false },
+    relay: { enabled: true, hop: { enabled: true, active: true } },
+    EXPERIMENTAL: { pubsub: true },
+    config: {
+      init: true,
+      Addresses: {
+        Swarm: env.pinner.Swarm,
+      },
+      Bootstrap: env.pinner.Bootstrap,
+    },
+  };
 
+  wrapperP = new Promise((resolve) =>
+    IPFS.create(ipfsJSConfig).then((ipfs) => {
+      const wrapper = new EveesPolkadotWrapper(ipfs, env.pinner);
+      resolve(wrapper);
+    })
+  );
   const rootEl = document.getElementById('root');
 
   render(<App />, rootEl);
